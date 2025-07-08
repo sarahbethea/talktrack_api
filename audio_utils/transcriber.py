dbafcd077fb81d9bee36519f7c7234f76fd5ae4a
Sarah Bethea <157849717+sarahbethea@users.noreply.github.com>
2025-07-08 13:23:37 -0600

    Update transcriber and test client

--- audio_utils/transcriber.py
+++ audio_utils/transcriber.py
@@ -1,31 +1,59 @@
-# Whisper transcription logic
-
-import whisper 
-import torch
 import time
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
-model = whisper.load_model("small", device=device) # Other options: "medium", "large"
+# device = "cuda" if torch.cuda.is_available() else "cpu"
+# model = whisper.load_model("small", device=device) # Other options: "medium", "large"
 
-print(f"Using device: {device}")
+# print(f"Using device: {device}")
 
-def transcribe_audio(file_path):
+def transcribe_with_whisper(model, file_path):
     """
     Transcribes audio using Whisper.
     
     Args:
+        model (WhisperModel): pretrained model.
         file_path (str): Path to audio file.
     
     Returns:
         dict: Contains full text and segment-level timestamps.
     """
     start_time = time.time()
-    result = model.transcribe(file_path)
+    result = model.transcribe(file_path) #run inference
     end_time = time.time()
     inference_time = end_time - start_time
 
     return {
         "text": result["text"],
         "segments": result["segments"],
+        "inference_time": round(inference_time, 2)
+    }
+
+
+def transcribe_with_faster_whisper(model, audio_path, word_timestamps=False):
+    """
+    Transcribes audio using a provided faster-whisper model.
+
+    Args:
+        model (WhisperModel): A preloaded faster-whisper model.
+        file_path (str): Path to audio file.
+        word_timestamps (bool): Whether to include word-level timing.
+
+    Returns:
+        dict: Contains list of segments and total inference time.
+    """
+    start_time = time.time()
+    segments, _ = model.transcribe(audio_path, word_timestamps=word_timestamps)
+    end_time = time.time()
+    inference_time = round(end_time - start_time, 2)
+
+    segment_list = []
+    for segment in segments:
+        segment_list.append({
+            "start": segment.start,
+            "end": segment.end,
+            "text": segment.text
+        })
+
+    return {
+        "segments": segment_list,
         "inference_time": round(inference_time, 2)
     }
--- tests/test_transcribe.py
+++ tests/test_transcribe.py
@@ -1,26 +1,80 @@
-from audio_utils.transcriber import transcribe_audio
+from audio_utils.transcriber import transcribe_with_whisper, transcribe_with_faster_whisper
+import torch
+import whisper
+from faster_whisper import WhisperModel
+import json
+import os
 
 # run with command: 
 # python -m tests.test_transcribe
 
 sample = "sample_1_9m"
 audio_path = f"data/raw/{sample}.wav"
-output_path = f"data/processed/test_transcript_{sample}.txt"
 
-result = transcribe_audio(audio_path)
+device = "cuda" if torch.cuda.is_available() else "cpu"
+compute_type = "float16" if device == "cuda" else "int8"
+    
 
-print(f"Transcription completed in {result['inference_time']} seconds")
+def test_w_transcription():
+    """
+    Test Whisper transcription
+    """
+    print(f"*** Testing Whisper. Using device: {device}")
+    
+    # Load model
+    model = whisper.load_model("small", device=device) # Other options: "medium", "large"
 
-with open(output_path, "w", encoding="utf-8") as f:
-    f.write("FULL TRANSCRIPT:\n")
-    f.write(result["text"])
-    f.write("\n\nSEGMENT TRANSCRIPTS:\n\n")
+    # Run inference
+    result = transcribe_with_whisper(model, audio_path) # Run inference
 
-    for segment in result["segments"]:
-        start = round(segment["start"], 2)
-        end = round(segment["end"], 2)
-        text = segment["text"]
+    print(f"Transcription completed in {result['inference_time']} seconds")
 
-        f.write(f"{start}s - {end}s {text}\n")
+    # Define output path for results
+    output_path = f"data/processed/test_transcript_{sample}_whisper.txt"
 
-print(f"Transcription written to {output_path}")
+    with open(output_path, "w", encoding="utf-8") as f:
+        f.write("FULL TRANSCRIPT:\n")
+        f.write(result["text"])
+        f.write("\n\nSEGMENT TRANSCRIPTS:\n\n")
+
+        for segment in result["segments"]:
+            start = round(segment["start"], 2)
+            end = round(segment["end"], 2)
+            text = segment["text"]
+
+            f.write(f"{start}s - {end}s {text}\n")
+
+    print(f"Transcription written to {output_path}")
+
+
+
+def test_fw_transcription():
+    """
+    Test Faster Whisper transcription
+    """
+    print(f"*** Testing Faster Whisper. Using device: {device}, compute_type: {compute_type}")
+
+    # Load model
+    model = WhisperModel("small", compute_type=compute_type, device=device)
+    
+    # Run inference, result will be type dictionary
+    result = transcribe_with_faster_whisper(model, audio_path)
+    
+    # Pull values out of result
+    inference_time = result["inference_time"]
+    segments = result["segments"]
+
+    # Define output path for results
+    output_path = f"data/processed/test_transcript_{sample}_faster_whisper.txt"
+
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+    with open(output_path, "w", encoding="utf-8") as f:
+        json.dump(result["segments"], f, indent=2, ensure_ascii=False)
+
+    print(f"Transcription written to {output_path}")
+    print(f"Inference time: {inference_time}")
+
+
+if __name__ == "__main__":
+    test_w_transcription()
+    test_fw_transcription()
